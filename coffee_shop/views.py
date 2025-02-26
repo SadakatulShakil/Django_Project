ee3311b05b75bd5d3219ea92860e19226ee2769b
Shakil <shakil@Shakils-MacBook-Pro.local>
2025-02-26 12:45:00 +0600

    retreive method added for specific item retreive

--- coffee_shop/views.py
+++ coffee_shop/views.py
@@ -26,6 +26,12 @@ class DrinksViewSet(viewsets.ModelViewSet):
                 status=status.HTTP_201_CREATED
             )
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+# Override the retreive method to customize the response
+    def retreive(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 # Override the update method to customize the response
     def update(self, request, *args, **kwargs):
